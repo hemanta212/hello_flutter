e89d5e833230f9d4457f6d5f3ede1008b4f60c06
hemanta212 <sharmahemanta.212@gmail.com>
2019-03-27 16:28:29 +0545

    added counter

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,30 +3,38 @@ import 'package:flutter/material.dart';
 class App extends StatefulWidget{
   createState() {
     return AppStateKeeper();
+
   }
 }
 
-
-
-
 class AppStateKeeper extends State<App> {
+
+  int Score = 1;
+
   final themeColor = const Color(0xFF26ae60);
   final bgColor = const Color(0xFFDAE0E2);
 
   Widget build(context) {
     return MaterialApp(
     home: Scaffold(
+        body: Text('$Score'),
+
         floatingActionButton: FloatingActionButton(
           child: Icon(
             Icons.add,
             color: bgColor,
           ),
           backgroundColor: themeColor,
-          onPressed : () {},
+          onPressed: () {
+            setState(() {
+              Score = Score + 1;
+            });
+          }
         ),
+
         backgroundColor: bgColor,
         appBar: AppBar(
-          title: Text('Instagram'),
+          title: Text('Sound Cloud'),
           backgroundColor: themeColor,
 
         ),
