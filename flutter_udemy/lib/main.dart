f04aef01d77f9d41e5276c905937c5a1ab4abf7b
hemanta212 <sharmahemanta.212@gmail.com>
2019-04-15 15:35:04 +0545

    completed long list view listview.builder

--- flutter_udemy/lib/main.dart
+++ flutter_udemy/lib/main.dart
@@ -9,7 +9,7 @@ void main() {
       home: Scaffold(
 
         appBar: AppBar(
-          title: Text("Basic List View")
+          title: Text("Long List View")
         ), // AppBar
 
         body: getListView(),
@@ -18,37 +18,28 @@ void main() {
   ); //runApp
 } // Main
 
+List<String> getListElements() {
+
+  var items = List<String>.generate(1000, (counter) => "Item $counter");
+  return items;
+}
+
 Widget getListView() {
 
-  var listView = ListView(
-    children: <Widget>[
+  var listItems = getListElements();
 
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beautiful View !"),
-        trailing: Icon(Icons.wb_sunny),
+  var listView = ListView.builder(
+    itemBuilder: (context, index) {
+
+      return ListTile(
+        leading: Icon(Icons.arrow_right),
+        title: Text(listItems[index]),
         onTap: () {
-          debugPrint("Landscape tapped");
+          debugPrint('${listItems[index]} was tapped');
         },
-      ),//ListTile
-
-      ListTile(
-        leading: Icon(Icons.laptop_chromebook),
-        title: Text("Windows"),
-      ),
-
-      ListTile(
-        leading: Icon(Icons.phone),
-        title: Text("Phone"),
-      ),
-
-      Text("Yet another element"),
-      Container(color: Colors.red, height: 50.0)
-
-    ],//Widget list
-
-  ); // ListView
+      ); // ListTile
+    }
+  );
 
   return listView;
 } // getListView
